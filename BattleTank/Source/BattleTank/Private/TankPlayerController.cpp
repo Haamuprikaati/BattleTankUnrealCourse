a0e6e77537bc5978a7afea2e0a78178f8faa41b2
Konsta Kontunen <konsta.kontunen@ubisoft.com>
2019-09-25 13:43:50 +0300

    Tick method and placeholder for crosshairaiming added in C++

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -3,7 +3,6 @@
 #include "TankPlayerController.h"
 #include "BattleTank.h"
 
-
 void ATankPlayerController::BeginPlay()
 {
 	Super::BeginPlay();
@@ -19,7 +18,24 @@ void ATankPlayerController::BeginPlay()
 	}
 }
 
+void ATankPlayerController::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	AimTowardsCrosshair();
+	UE_LOG(LogTemp, Warning, TEXT("PlayerController ticking"));
+
+}
+
 ATank* ATankPlayerController::GetControlledTank() const
 {
 	return Cast<ATank>(GetPawn());
+}
+
+void ATankPlayerController::AimTowardsCrosshair()
+{
+	if (!GetControlledTank()) { return; }
+
+	//Get world location if linetrace through crosshair
+	//If it hits the landscape
+	//Tell controlled tank to aim at this point
 }
