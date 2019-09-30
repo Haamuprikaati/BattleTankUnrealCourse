6d807f55e8e0d4f3429356085fc8a9aa5a4a5a32
Konsta Kontunen <konsta.kontunen@ubisoft.com>
2019-09-30 20:15:46 +0300

    Completing Manual Tank Movement

--- BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
@@ -2,9 +2,31 @@
 
 
 #include "TankMovementComponent.h"
+#include "TankTrack.h"
 
+
+void UTankMovementComponent::Initialise(UTankTrack* LeftTrackToSet, UTankTrack* RightTrackToSet)
+{
+	LeftTrack = LeftTrackToSet;
+	RightTrack = RightTrackToSet;
+}
 
 void UTankMovementComponent::IntendMoveForward(float Throw)
 {
-	UE_LOG(LogTemp, Warning, TEXT("Intend move forward throw: %f"), Throw);
+	//UE_LOG(LogTemp, Warning, TEXT("Intend move forward throw: %f"), Throw)
+	if (!LeftTrack || !RightTrack) { return; }
+	LeftTrack->SetThrottle(Throw);
+	RightTrack->SetThrottle(Throw);
+	//TODO prevent double speed due to multiple control systems
+
+}
+
+void UTankMovementComponent::IntendTurnRight(float Throw)
+{
+	//UE_LOG(LogTemp, Warning, TEXT("Intend move forward throw: %f"), Throw)
+	if (!LeftTrack || !RightTrack) { return; }
+	LeftTrack->SetThrottle(Throw);
+	RightTrack->SetThrottle(-Throw);
+	//TODO prevent double speed due to multiple control systems
+
 }
