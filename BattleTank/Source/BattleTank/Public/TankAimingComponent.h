d70639ff963f04095234e80bcfa5d6dffec7dea7
Konsta Kontunen <konsta.kontunen@ubisoft.com>
2019-10-01 14:14:16 +0300

    Refactoring Our Aiming Component

--- BattleTank/Source/BattleTank/Public/TankAimingComponent.h
+++ BattleTank/Source/BattleTank/Public/TankAimingComponent.h
@@ -8,6 +8,15 @@
 #include "Kismet/GameplayStatics.h"
 #include "TankAimingComponent.generated.h"
 
+//Enum for aiming states
+UENUM()
+enum class EFiringState : uint8
+{
+	Reloading, 
+	Aiming,
+	Locked
+};
+
 //Forward declaration
 class UTankBarrel;
 class UTankTurret;
@@ -29,6 +38,10 @@ public:
 
 	void AimAt(FVector OutHitLocation, float LaunchSpeed);
 
+protected:
+	UPROPERTY(BlueprintReadOnly, Category = "State")
+	EFiringState FiringState = EFiringState::Reloading;
+
 private:
 	UTankBarrel* Barrel = nullptr;
 		
@@ -37,4 +50,5 @@ private:
 	UTankTurret* Turret = nullptr;
 
 	void MoveTurretTowards(FVector AimDirection);
+
 };
